418f68b7fd47e1a1360bf9fe2b3926b558d7e2a1
MurtazaN <63307199+MurtazaN@users.noreply.github.com>
2026-03-26 13:24:01 -0400

    Update main.tf

--- deployment/terraform/environments/dev/main.tf
+++ deployment/terraform/environments/dev/main.tf
@@ -1,8 +1,10 @@
 terraform {
   required_version = ">= 1.5"
   required_providers {
-    google = { source = "hashicorp/google"; version = "~> 5.0" }
-    random = { source = "hashicorp/random"; version = "~> 3.6" }
+    google = { source = "hashicorp/google"
+      version = "~> 5.0" }
+    random = { source = "hashicorp/random"
+      version = "~> 3.6" }
   }
 }
 

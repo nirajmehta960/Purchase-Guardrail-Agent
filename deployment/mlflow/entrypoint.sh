c3d0507773b76bcdbff59d199e107d5ac03b0265
Niraj Mehta <nirajmehta960@gmail.com>
2026-04-12 12:11:13 -0400

    chore: update MLflow entrypoint for robust startup

--- deployment/mlflow/entrypoint.sh
+++ deployment/mlflow/entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
 
-# Build PostgreSQL URI from Cloud Run env vars injected by Terraform.
-# DB_HOST is the Unix socket path: /cloudsql/PROJECT:REGION:INSTANCE
-BACKEND_URI="postgresql://${DB_USER}:${DB_PASS}@/${DB_NAME}?host=${DB_HOST}"
+# URL-encode the password so special characters (], {, ), etc.) don't break
+# Python's urlparse when MLflow parses the backend store URI.
+DB_PASS_ENCODED=$(python3 -c "import urllib.parse, os; print(urllib.parse.quote(os.environ['DB_PASS'], safe=''))")
+
+# DB_HOST is the Cloud SQL Unix socket dir: /cloudsql/PROJECT:REGION:INSTANCE
+BACKEND_URI="postgresql://${DB_USER}:${DB_PASS_ENCODED}@/${DB_NAME}?host=${DB_HOST}"
 
 exec mlflow server \
   --backend-store-uri "${BACKEND_URI}" \
